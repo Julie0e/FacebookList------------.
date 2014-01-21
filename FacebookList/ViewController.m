7004743be0327741143a8c6d09232958e66e178c
Julie0e <julie_91@hanmail.net>
2014-01-21 18:26:52 +0900

    SDK로....친구목록 추가했습니다....

--- FacebookList/ViewController.m
+++ FacebookList/ViewController.m
@@ -7,11 +7,8 @@
 //
 
 #import "ViewController.h"
-#import <Social/Social.h>
-#import <Accounts/Accounts.h>
 #import <FacebookSDK/FacebookSDK.h>
 
-#define FACEBOOK_APPID @"1405220066393435"
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 
@@ -19,119 +16,64 @@
 @property (strong, nonatomic)NSArray *data;
 
 @property (weak, nonatomic) IBOutlet UITableView *table;
+@property (weak, nonatomic) IBOutlet UIImageView *image;
 
 @end
 
 @implementation ViewController
-
-- (void)showFriendsList
 {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ACAccountStore *store = [[ACAccountStore alloc] init];
-    ACAccountType *accountType = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
-    NSDictionary *options = @{ACFacebookAppIdKey:FACEBOOK_APPID,
-                              ACFacebookPermissionsKey:@[@"read_friendlists"],
-                              ACFacebookAudienceKey:ACFacebookAudienceEveryone};
-    [store requestAccessToAccountsWithType:accountType options:options completion:^(BOOL granted, NSError *error) {
-        if (error) {
-            NSLog(@"Error:%@", error);
-        }
-        if (granted) {
-            NSLog(@"권한 승인 성공");
-            NSArray *accountList = [store accountsWithAccountType:accountType];
-            self.facebookAccount = [accountList lastObject];
-            
-            [self requestFriendsList];
-        }
-        else
-        {
-            NSLog(@"권한 승인 실패");
-        }
+    NSArray *friends;
+}
+
+
+-(void)loginViewShowingLoggedInUser:(FBLoginView *)loginView
+{
+    FBRequest* friendsRequest = [FBRequest requestForMyFriends];
+    [friendsRequest startWithCompletionHandler: ^(FBRequestConnection *connection,
+                                                  NSDictionary* result,
+                                                  NSError *error) {
+        friends = [result objectForKey:@"name"];
+        [self.table reloadData];
     }];
 }
 
-- (void)requestFriendsList
+-(void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView
 {
-    NSString *urlStr = @"https://graph.facebook.com/me/friends";
-    NSURL *url = [NSURL URLWithString:urlStr];
-    NSDictionary *params = nil;
-    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook requestMethod:SLRequestMethodGET URL:url parameters:params];
-    request.account = self.facebookAccount;
-    
-    [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
-        if (error != nil) {
-            NSLog(@"Error : %@", error);
-            return;
-        }
-        
-        __autoreleasing NSError *parseError;
-        NSDictionary *result = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&parseError];
-        
-        self.data = result[@"data"];
-        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-            [self.table reloadData];
-        }];
-    }];
+    friends = nil;
+    [self.table reloadData];
 }
+
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.data count];
+    return [friends count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"LIST_CELL"];
-    NSDictionary *one = self.data[indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"LIST_CELL" forIndexPath:indexPath];
     
-    NSString *contents;
-    if (one[@"name"]) {
-        contents = [NSString stringWithFormat:@"%@", one[@"name"]];
-    }
-    else
-    {
-        contents = one[@"story"];
-        cell.indentationLevel = 2;
-    }
+    NSDictionary<FBGraphUser> *friendList;
+    friendList = friends[indexPath.row];
     
-    cell.self.textLabel.text = contents;
+    
+    cell.textLabel.text = friendList.name;
+    
     return cell;
 
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [self showFriendsList];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    friends = [[NSArray alloc]init];
+    
+    
+    FBLoginView *loginView = [[FBLoginView alloc] init];
+    // Align the button in the center horizontally
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 50);
+    loginView.delegate = self;
+    [self.view addSubview:loginView];
 }
 
 - (void)didReceiveMemoryWarning
