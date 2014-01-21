1c405bf01f447ae990c2776e2106c10c2db9df3f
Julie0e <julie_91@hanmail.net>
2014-01-21 15:08:17 +0900

    Initial Commit

---
+++ FacebookList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookList
+//
+//  Created by 주리 on 2014. 1. 21..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FacebookList
+//
+//  Created by 주리 on 2014. 1. 21..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
